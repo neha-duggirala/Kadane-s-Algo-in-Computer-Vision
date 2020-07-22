90fca144e1ceaf7028c323ad49391519a2491f4e
neha-duggirala <nsm.duggirala@gmail.com>
2020-07-22 22:13:47 +0530

    implement brute force

--- Brute_force.py
+++ Brute_force.py
@@ -1,1 +1,10 @@
-print("Brute Force")
+array =[-2, 1]
+size = len(array)
+max_ = array[0]
+for idx1 in range(size):
+    for idx2 in range(idx1+1,size+1):
+        # print(sum(array[idx1:idx2]),(array[idx1:idx2]))
+        max_ = max(sum(array[idx1:idx2]),max_)
+
+
+print(max_)
